8b4839c9d5318eb165245d5b992c6f1253efc456
Mike Mackrory <mike@echovue.com>
2018-04-24 23:15:59 -0700

    Update to match requirements

--- lambda/lambdahandler.py
+++ lambda/lambdahandler.py
@@ -1,5 +1,17 @@
-# Main entry method for lambda
+from __future__ import division
+
 def lambda_handler(event, context):
-    value = event['initialValue']
-    value2 = value + 1
-    return {"initialValue": value, "incrementedValue": value2}
+    number1 = event['Number1']
+    number2 = event['Number2']
+    sum = number1 + number2
+    product = number1 * number2
+    difference = abs(number1 - number2)
+    quotient = number1 / number2
+    return {
+            "Number1": number1,
+            "Number2": number2,
+            "Sum": sum,
+            "Product": product,
+            "Difference": difference,
+            "Quotient": quotient
+            }
